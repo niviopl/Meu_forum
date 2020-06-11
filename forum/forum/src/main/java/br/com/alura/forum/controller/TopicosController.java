4f07495b6341b4cb47be8cc7a94735b0cd99b94e
niviopl <nivioplima@yahoo.com>
2020-06-10 22:58:46 -0300

    Criacao de DTO na API Meu_Forum

---
+++ forum/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,26 @@
+package br.com.alura.forum.controller;
+
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController     // substitui o Response Body no inicio de todos os métodos
+public class TopicosController {
+
+    @RequestMapping("/topicos")
+    //@ResponseBody
+    public List<TopicoDto> lista(){
+        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+
+        return TopicoDto.converter(Arrays.asList(topico,topico,topico));
+    }
+
+}
