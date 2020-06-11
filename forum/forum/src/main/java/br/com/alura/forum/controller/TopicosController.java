b6aaa46a427f694a64801ec7a1ade16f6345f30d
niviopl <nivioplima@yahoo.com>
2020-06-11 17:57:52 -0300

    Prevendo mensagens de exceções

--- forum/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
@@ -38,7 +39,7 @@ public class TopicosController {
         }
     }
     @PostMapping
-    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBilder){
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBilder){
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
         URI uri = uriBilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
