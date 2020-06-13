98eec4067b8f1cf5b07d4862969678f39f40bb93
niviopl <nivioplima@yahoo.com>
2020-06-13 18:46:47 -0300

    Protegendo com Spring Security

--- forum/forum/src/main/resources/data.sql
+++ forum/forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$8pCwtvNFUrU8.o3SDMvP5O1pYhYYxDsdpzl0IBwZoB3LsyMNhcLOO');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
