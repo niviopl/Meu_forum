1ca07f3ebe7f7cad7b0a28a8f8c8f9b7d018443a
niviopl <nivioplima@yahoo.com>
2020-06-13 15:22:44 -0300

    Inicio Spring Boot 2 - Prevendo paginacao

--- forum/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,9 +1,11 @@
 package br.com.alura.forum.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.alura.forum.modelo.Topico;
 import java.util.List;
 
 public interface TopicoRepository extends JpaRepository <Topico, Long> {
-    List<Topico> findByCursoNome (String nomeCurso);
+    Page<Topico> findByCursoNome (String nomeCurso, Pageable paginacao);
 }
